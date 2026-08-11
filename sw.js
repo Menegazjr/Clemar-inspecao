3072e8b80db256394bd11452a6aa96ee06b28a3f
João da Silva Menegaz Júnior <31959417+Menegazjr@users.noreply.github.com>
2026-08-11 15:32:51 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Versão do cache — altere este número a cada deploy para forçar atualização
-const VERSION = '1.9.5';
+const VERSION = '1.9.6';
 const CACHE = `clemar-${VERSION}`;
 
 const ASSETS = [
